dd52220e53eecc8a72ae588ce35b11418df206f8
jlkim909 <jlkim909@naver.com>
2022-06-13 01:09:06 +0900

    edit joinUser

--- src/pages/JoinUser.jsx
+++ src/pages/JoinUser.jsx
@@ -1,4 +1,11 @@
-import { Alert, Box, Container, Grid, TextField } from "@mui/material";
+import {
+  Alert,
+  Box,
+  Container,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
 import React, { useCallback, useEffect, useState } from "react";
 import { LoadingButton } from "@mui/lab";
 import { Link } from "react-router-dom";
@@ -11,7 +18,6 @@ import {
 import { getDatabase, ref, set } from "firebase/database";
 import { useDispatch } from "react-redux";
 import { setUser } from "../store/userReducer";
-import logoImg from "../image/loginlogo.png";
 
 const IsPasswordValid = (password, confirmPassword) => {
   if (password.length < 6 || confirmPassword.length < 6) {
@@ -86,108 +92,138 @@ function JoinUser() {
     }, 3000);
   }, [error]);
   return (
-    <Container
-      component="main"
-      maxWidth="xs"
-      sx={{ backgroundColor: "#F6F6F6" }}
-    >
+    <>
       <Box
         sx={{
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
           justifyContent: "center",
-          height: "100vh",
+          height: "10vh",
+          width: "100%",
+          color: "white",
+          fontSize: "1.5rem",
+          borderRadius: "0 0 8px 8px",
+          backgroundColor: "#50accb",
         }}
       >
-        <img src={logoImg} alt="" style={{ marginBottom: "10vh" }} />
-        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                name="name"
-                required
-                fullWidth
-                label="이름을 입력해주세요."
-                variant="standard"
-                autoFocus
-                autoComplete="off"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                name="email"
-                required
-                fullWidth
-                label="이메일 주소"
-                variant="standard"
-                autoComplete="off"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                name="phoneNum"
-                required
-                fullWidth
-                label="전화번호"
-                variant="standard"
-                type="number"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                name="password"
-                required
-                fullWidth
-                variant="standard"
-                label="비밀번호"
-                type="password"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                name="confirmPassword"
-                required
-                fullWidth
-                variant="standard"
-                label="확인 비밀번호"
-                type="password"
-              />
-            </Grid>
-          </Grid>
-          {error ? (
-            <Alert sx={{ mt: 3 }} severity="error">
-              {error}
-            </Alert>
-          ) : null}
-          <LoadingButton
-            type="submit"
-            fullWidth
-            variant="contained"
-            loading={loading}
-            sx={{
-              mt: 3,
-              mb: 2,
-              backgroundColor: "#ECE6CC",
-              color: "#707070",
-              height: "16%",
-            }}
-          >
-            회원가입
-          </LoadingButton>
-          <Grid container justifyContent="flex-end">
-            <Grid item>
-              <Link
-                to="/login"
-                style={{ textDecoration: "none", color: "blue" }}
-              >
-                이미 계정이 있나요? 로그인으로 이동
-              </Link>
-            </Grid>
-          </Grid>
-        </Box>
+        회원가입
       </Box>
-    </Container>
+      <Container
+        component="main"
+        maxWidth="xs"
+        sx={{ backgroundColor: "#F6F6F6" }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "90vh",
+          }}
+        >
+          <Box component="form" noValidate onSubmit={handleSubmit}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Typography
+                  variant="h6"
+                  component="h2"
+                  fontSize="1.5rem"
+                  fontWeight="bold"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                >
+                  사용자 정보 입력
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  name="name"
+                  required
+                  fullWidth
+                  label="이름을 입력해주세요."
+                  variant="standard"
+                  autoFocus
+                  autoComplete="off"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  name="email"
+                  required
+                  fullWidth
+                  label="이메일 주소"
+                  variant="standard"
+                  autoComplete="off"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  name="phoneNum"
+                  required
+                  fullWidth
+                  label="전화번호"
+                  variant="standard"
+                  type="number"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  name="password"
+                  required
+                  fullWidth
+                  variant="standard"
+                  label="비밀번호"
+                  type="password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  name="confirmPassword"
+                  required
+                  fullWidth
+                  variant="standard"
+                  label="확인 비밀번호"
+                  type="password"
+                />
+              </Grid>
+            </Grid>
+            {error ? (
+              <Alert sx={{ mt: 3 }} severity="error">
+                {error}
+              </Alert>
+            ) : null}
+            <LoadingButton
+              type="submit"
+              fullWidth
+              variant="contained"
+              loading={loading}
+              sx={{
+                mt: 3,
+                mb: 2,
+                backgroundColor: "#ECE6CC",
+                color: "#707070",
+                height: "16%",
+              }}
+            >
+              회원가입
+            </LoadingButton>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link
+                  to="/login"
+                  style={{ textDecoration: "none", color: "blue" }}
+                >
+                  이미 계정이 있나요? 로그인으로 이동
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </>
   );
 }
 
